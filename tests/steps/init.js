6d9e826b687298482cc77de63c1c1e8d50b59140
krzysztofmirek <krzysztof.mirek@gmail.com>
2018-12-01 14:16:57 +0200

    add tests

---
+++ tests/steps/init.js
@@ -0,0 +1,20 @@
+let initialized = false
+
+const init = async () => {
+    if (initialized) {
+        return
+    }
+
+    process.env.restaurants_api   = "https://5zqiz1hdgg.execute-api.eu-west-1.amazonaws.com/dev/restaurants"
+    process.env.restaurants_table = "restaurants-dev-devternity"
+    process.env.AWS_REGION        = "eu-west-1"
+    process.env.order_events_stream = 'orders-dev-devternity'
+    process.env.restaurant_notification_topic = 'restaurants-dev-devternity'
+
+    initialized = true
+}
+
+module.exports = {
+    init
+}
+
