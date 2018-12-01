1839cdc8f4260a34a13a5166d1c3d14e65d546b7
krzysztofmirek <krzysztof.mirek@gmail.com>
2018-12-01 14:55:32 +0200

    update the endpoint

--- tests/steps/init.js
+++ tests/steps/init.js
@@ -5,7 +5,7 @@ const init = async () => {
         return
     }
 
-    process.env.restaurants_api   = "https://5zqiz1hdgg.execute-api.eu-west-1.amazonaws.com/dev/restaurants"
+    process.env.restaurants_api   = "https://5zqiz1hdgg.execute-api.eu-west-1.amazonaws.com/dev/"
     process.env.restaurants_table = "restaurants-dev-devternity"
     process.env.AWS_REGION        = "eu-west-1"
     process.env.order_events_stream = 'orders-dev-devternity'
